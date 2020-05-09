2395a954461dc6ff4ff238103f736400c54246df
John DeRiggi <john.deriggi@gmail.com>
2020-05-09 12:47:49 -0700

    adds tests for username uniqueness

--- tests/models_tests/user_test.py
+++ tests/models_tests/user_test.py
@@ -5,6 +5,10 @@ class UserTest(OhmTestCase):
     def test_get_multi(self):
         assert self.chuck.get_multi("PHONE") == ['+14086441234', '+14086445678']
         assert self.justin.get_multi("PHONE") == []
+
+        # confirm that data conforms to unique constraints in db
+        assert self.justin.username !=  self.chuck.username
+        assert self.chuck.username !=  self.elvis.username 
     
     def test_tiers(self):
         assert self.chuck.is_below_tier('Bronze')
